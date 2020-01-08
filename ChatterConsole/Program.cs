59ec5dad696cf9f8e6ef412a17d434f42a99c3cd
Blink The Things <blinkthethings@gmail.com>
2020-01-07 21:05:01 -0500

    Ask for IP address and add some comments

--- ChatterConsole/Program.cs
+++ ChatterConsole/Program.cs
@@ -8,6 +8,23 @@ namespace ChatterConsole
     {
         static void Main(string[] args)
         {
+            // Get the local IP Address to use
+            string ipAddress = "";
+            if (args.Length > 0)
+            {
+                ipAddress = args[0];
+            }
+            if (string.IsNullOrEmpty(ipAddress))
+            {
+                Console.Write("Enter the local IP address to use: ");
+                ipAddress = Console.ReadLine();
+                while(string.IsNullOrEmpty(ipAddress))
+                {
+                    ipAddress = Console.ReadLine();
+                }
+            }
+
+            // Get the display name to use
             string displayName = "";
             if (args.Length > 1)
             {
@@ -23,26 +40,30 @@ namespace ChatterConsole
                 }
             }
 
-            var ChatterNode = new Chatter.Node(IPAddress.Parse(args[0]), IPAddress.Parse("239.255.10.11"), 1314);
+            // Create a new Chatter node
+            var chatterNode = new Chatter.Node(IPAddress.Parse(ipAddress), IPAddress.Parse("239.255.10.11"), 1314);
 
-            ChatterNode.MessageReceivedEventHandler += (sender, m) =>
+            // Attach a message handler
+            chatterNode.MessageReceivedEventHandler += (sender, m) =>
             {
                 HandleMessagingCalls(m, displayName);
             };
 
+            // Start task to receive messages
             Task.Run(() =>
             {
-                ChatterNode.StartReceiving();
+                chatterNode.StartReceiving();
             });
 
+            // Get messages and send them out
             Console.Write("> ");
             string message = Console.ReadLine();
 
-            while (message.ToLower() != "quit")
+            while (message.ToLower() != "/quit")
             {
                 if (!string.IsNullOrEmpty(message))
                 {
-                    ChatterNode.Send(displayName + ">" + message);
+                    chatterNode.Send(displayName + ">" + message);
                 }
 
                 Console.Write("\n> ");
@@ -52,18 +73,22 @@ namespace ChatterConsole
 
         static void HandleMessagingCalls(Chatter.MessageReceivedEventArgs m, string displayName)
         {
+            // Check to see if this is a private message
             string outputMessage = m.Message;
             if (outputMessage.StartsWith("/pm "))
             {
+                // Check to see if this private message is to me
                 outputMessage = outputMessage.Substring(4).Trim();
                 if (outputMessage.StartsWith(displayName + " "))
                 {
+                    // Display the message
                     outputMessage = outputMessage.Substring(displayName.Length);
                     Console.WriteLine($"\n< { m.SenderName }: {outputMessage} \n> ");
                 }
             }
             else
             {
+                // Display the message
                 Console.WriteLine($"\n< { m.SenderName }: {m.Message} \n> ");
             }
         }
