a358d7275ae46e168a3ce2481349b1719202d570
Blink The Things <blinkthethings@gmail.com>
2020-01-11 14:13:07 -0500

    Improvments in Client

--- Chatter/Client.cs
+++ Chatter/Client.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Primary udp messaging clien class
+Primary UDP messaging client class
 Copyright (C) 2020  Trash Bros (BlinkTheThings, Reakain)
 
 This program is free software: you can redistribute it and/or modify
@@ -22,25 +22,50 @@ using System.Text;
 
 namespace Chatter
 {
-    public class MessageReceivedEventArgs
+    public class MessageReceivedEventArgs : EventArgs
     {
-        public string Message { get; }
-        public IPAddress SenderIP { get; }
+        public string Message { get; set; }
+        public IPAddress SenderIP { get; set; }
         public MessageReceivedEventArgs(string message, IPAddress senderIP)
         {
             Message = message;
             SenderIP = senderIP;
         }
     }
-    public class Client
+
+    public class AlreadyReceivingException : Exception
     {
+        public AlreadyReceivingException()
+        {
+        }
+
+        public AlreadyReceivingException(string message)
+            : base(message)
+        {
+        }
+
+        public AlreadyReceivingException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+    public class Client : IDisposable
+    {
+        #region Properties
         public IPAddress LocalIP { get; }
         public IPAddress MulticastIP { get; }
         public int Port { get; }
+        #endregion
 
+        #region Events
         public event EventHandler<MessageReceivedEventArgs> MessageReceivedEventHandler;
+        #endregion
 
-        private bool m_isReceiving = false;
+        #region Private member variables
+        private Socket _receiveSocket;
+        private bool _disposed = false;
+        #endregion
 
         public Client(IPAddress localIP, IPAddress multicastIP, int port)
         {
@@ -51,48 +76,87 @@ namespace Chatter
 
         public void StartReceiving()
         {
-            m_isReceiving = true;
-            using (var sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            if (_receiveSocket != null)
             {
-                sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-                sock.Bind(new IPEndPoint(IPAddress.Any, Port));
-                sock.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(MulticastIP, LocalIP));
+                throw new AlreadyReceivingException();
+            }
 
-                IPEndPoint remoteIPEndpoint = new IPEndPoint(MulticastIP, 0);
-                EndPoint remoteEndPoint = remoteIPEndpoint;
-
-                try
+            try
+            {
+                using (_receiveSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
                 {
-                    while (m_isReceiving)
+                    _receiveSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                    _receiveSocket.Bind(new IPEndPoint(IPAddress.Any, Port));
+                    _receiveSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(MulticastIP, LocalIP));
+
+                    IPEndPoint remoteIPEndPoint = new IPEndPoint(MulticastIP, 0);
+                    EndPoint remoteEndPoint = remoteIPEndPoint;
+
+                    while (true)
                     {
                         byte[] datagram = new byte[65536];
-                        int length = sock.ReceiveFrom(datagram, 0, datagram.Length, SocketFlags.None, ref remoteEndPoint);
+                        int length = _receiveSocket.ReceiveFrom(datagram, 0, datagram.Length, SocketFlags.None, ref remoteEndPoint);
                         Array.Resize(ref datagram, length);
 
                         string message = Encoding.UTF8.GetString(Convert.FromBase64String(Encoding.UTF8.GetString(datagram)));
-                        MessageReceivedEventHandler?.Invoke(this, new MessageReceivedEventArgs(message, remoteIPEndpoint.Address));
+                        MessageReceivedEventHandler?.Invoke(this, new MessageReceivedEventArgs(message, remoteIPEndPoint.Address));
                     }
                 }
-                catch (SocketException e)
-                {
-                    Console.WriteLine(e);
-                }
             }
-            m_isReceiving = false;
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
         }
 
-        public void Dispose()
+        public void StopReceiving()
         {
-            m_isReceiving = false;
+            try
+            {
+                _receiveSocket?.Shutdown(SocketShutdown.Receive);
+                _receiveSocket?.Close();
+                _receiveSocket = null;
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
         }
 
         public void Send(string message)
         {
-            using (var udpClient = new UdpClient(new IPEndPoint(LocalIP, 0)))
+            try
             {
-                byte[] datagram = Encoding.UTF8.GetBytes(Convert.ToBase64String(Encoding.UTF8.GetBytes(message)));
-                udpClient.Send(datagram, datagram.Length, new IPEndPoint(MulticastIP, Port));
+                using (var udpClient = new UdpClient(new IPEndPoint(LocalIP, 0)))
+                {
+                    byte[] datagram = Encoding.UTF8.GetBytes(Convert.ToBase64String(Encoding.UTF8.GetBytes(message)));
+                    udpClient.Send(datagram, datagram.Length, new IPEndPoint(MulticastIP, Port));
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
             }
         }
+
+        #region IDisposable Support
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    StopReceiving();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
     }
 }
