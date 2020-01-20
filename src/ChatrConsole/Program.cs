02747f0e2a055534c29512abf7ceb6675114d018
Blink The Things <blinkthethings@gmail.com>
2020-01-19 20:57:58 -0500

    Use resource file to fix CA1303, closes #26

--- src/ChatrConsole/Program.cs
+++ src/ChatrConsole/Program.cs
@@ -25,36 +25,55 @@ namespace ChatrConsole
 {
     internal class Program
     {
-        #region Fields
+        #region Private Fields
 
-        private static string s_currentInput = string.Empty;
-        private static string s_nextInput = string.Empty;
+        private static readonly string Prompt = SR.Prompt;
         private static readonly List<string> s_messageHistory = new List<string>();
-        private static int s_historyIndex = -1;
         private static Chatr.MultiChannel s_chatrClient;
+        private static string s_currentInput = string.Empty;
+        private static int s_historyIndex = -1;
+        private static string s_nextInput = string.Empty;
 
-        #endregion Fields
+        #endregion Private Fields
+
+        #region Private Methods
 
         /// <summary>
-        /// Entry point of the program.
+        /// Clear any user message input from the display
         /// </summary>
-        /// <param name="args">Command line arguments</param>
-        private static void Main(string[] args)
+        private static void ClearInputLines()
         {
-            // Display the assembly version information
-            DisplayVersion();
+            Console.SetCursorPosition(0, Console.CursorTop - 1 * ((s_currentInput.Length + 1) / Console.WindowWidth));
+            Console.Write(new string(' ', s_currentInput.Length + 2));
+            Console.SetCursorPosition(0, Console.CursorTop - 1 * ((s_currentInput.Length + 1) / Console.WindowWidth));
+        }
 
-            // Try and read the settings path from the command line arguments
-            string settingsPath = (args == null || args.Length == 0) ? null : args[0];
+        /// <summary>
+        /// Display the message input from the user
+        /// </summary>
+        private static void DisplayInput()
+        {
+            // Clear the previous message input
+            ClearInputLines();
 
-            // Startup the Chatr client
-            StartupChatr(GetSettingsPath(settingsPath));
+            // Display the new message input
+            Console.Write(Prompt + s_nextInput);
+        }
 
-            // Read and process commands entered by the user until the quit message is entered
-            ReadAndProcessMessagesUntilQuit();
-
-            // Shutdown the Chatr client
-            ShutdownChatr();
+        /// <summary>
+        /// Display a recevied message
+        /// </summary>
+        /// <param name="message">Message to display</param>
+        private static void DisplayMessage(string message, string textColor)
+        {
+            ClearInputLines();
+            if (Enum.TryParse<ConsoleColor>(textColor, true, out ConsoleColor consoleColor))
+            {
+                Console.ForegroundColor = consoleColor;
+            }
+            Console.Write(message);
+            Console.ResetColor();
+            Console.Write(Environment.NewLine + Prompt + s_nextInput);
         }
 
         /// <summary>
@@ -70,90 +89,7 @@ namespace ChatrConsole
         }
 
         /// <summary>
-        ///  Start a new Chatr client using the settings from <paramref name="settingsPath"/>
-        /// </summary>
-        /// <param name="settingsPath">File path to chatr settings file</param>
-        private static void StartupChatr(string settingsPath)
-        {
-            // Create a new Chatr client
-            s_chatrClient = new Chatr.MultiChannel(settingsPath);
-
-            // Attach a message display handler
-            s_chatrClient.MessageDisplayEventHandler += (sender, m) =>
-            {
-                DisplayMessage(m[0], m[1]);
-            };
-        }
-
-        /// <summary>
-        /// Read messages entered by the user and send them to the client util the user enters the quit message
-        /// </summary>
-        private static void ReadAndProcessMessagesUntilQuit()
-        {
-            // Display intial prompt
-            Console.Write("\n> ");
-
-            // Read the first message
-            string message = ReadMessage();
-
-            // Loop while it isn't the quit message
-            while (!IsQuitMessage(message))
-            {
-                // Try and process it as console command first
-                if (!TryProcessAsConsoleCommand(message))
-                {
-                    // Send the message to the client
-                    s_chatrClient.SendMessage(message);
-                }
-
-                // Read the next message
-                message = ReadMessage();
-            };
-        }
-
-        /// <summary>
-        /// Try to process the message as a console command
-        /// </summary>
-        /// <param name="message">Message to process</param>
-        /// <returns>true if the command was handled, otherwise false</returns>
-        private static bool TryProcessAsConsoleCommand(string message)
-        {
-            // Check for empty message
-            if (string.IsNullOrEmpty(message))
-            {
-                // Do nothing
-
-                // command handled
-                return true;
-            }
-
-            // Check for the clear command
-            if (message.StartsWith("/clear", true, CultureInfo.CurrentCulture))
-            {
-                // Clear the console and display the prompt
-                Console.Clear();
-                Console.Write("> ");
-
-                // command handled
-                return true;
-            }
-
-            // command not handled
-            return false;
-        }
-
-        /// <summary>
-        /// Shutdown the Chatr client
-        /// </summary>
-        private static void ShutdownChatr()
-        {
-            s_chatrClient?.ShutDown();
-            s_chatrClient = null;
-        }
-
-        /// <summary>
-        /// Get the settings path based on a suggested settings path
-        /// Use the default path if invalid
+        /// Get the settings path based on a suggested settings path Use the default path if invalid
         /// </summary>
         /// <param name="settingsPath">Settings path to use</param>
         /// <returns></returns>
@@ -181,6 +117,66 @@ namespace ChatrConsole
         {
             bool isValidUri = Uri.TryCreate(pathString, UriKind.Absolute, out Uri pathUri);
             return isValidUri && pathUri != null && pathUri.IsLoopback;
+        }
+
+        /// <summary>
+        /// Check to see if a message is the quit message
+        /// </summary>
+        /// <param name="message">Message to check</param>
+        /// <returns></returns>
+        private static bool IsQuitMessage(string message)
+        {
+            string text = message.ToLower(System.Globalization.CultureInfo.CurrentCulture).TrimStart('/');
+            return (text == Chatr.CommandList.QUIT || text == Chatr.CommandList.QUIT_S);
+        }
+
+        /// <summary>
+        /// Entry point of the program.
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
+        private static void Main(string[] args)
+        {
+            // Display the assembly version information
+            DisplayVersion();
+
+            // Try and read the settings path from the command line arguments
+            string settingsPath = (args == null || args.Length == 0) ? null : args[0];
+
+            // Startup the Chatr client
+            StartupChatr(GetSettingsPath(settingsPath));
+
+            // Read and process commands entered by the user until the quit message is entered
+            ReadAndProcessMessagesUntilQuit();
+
+            // Shutdown the Chatr client
+            ShutdownChatr();
+        }
+
+        /// <summary>
+        /// Read messages entered by the user and send them to the client util the user enters the
+        /// quit message
+        /// </summary>
+        private static void ReadAndProcessMessagesUntilQuit()
+        {
+            // Display intial prompt
+            Console.Write(Environment.NewLine + Prompt);
+
+            // Read the first message
+            string message = ReadMessage();
+
+            // Loop while it isn't the quit message
+            while (!IsQuitMessage(message))
+            {
+                // Try and process it as console command first
+                if (!TryProcessAsConsoleCommand(message))
+                {
+                    // Send the message to the client
+                    s_chatrClient.SendMessage(message);
+                }
+
+                // Read the next message
+                message = ReadMessage();
+            };
         }
 
         /// <summary>
@@ -276,7 +272,7 @@ namespace ChatrConsole
             ClearInputLines();
 
             // Display input prompt
-            Console.Write("> ");
+            Console.Write(Prompt);
 
             // Clear the input strings
             s_currentInput = string.Empty;
@@ -287,53 +283,61 @@ namespace ChatrConsole
         }
 
         /// <summary>
-        /// Check to see if a message is the quit message
+        /// Shutdown the Chatr client
         /// </summary>
-        /// <param name="message">Message to check</param>
-        /// <returns></returns>
-        private static bool IsQuitMessage(string message)
+        private static void ShutdownChatr()
         {
-            string text = message.ToLower(System.Globalization.CultureInfo.CurrentCulture).TrimStart('/');
-            return (text == Chatr.CommandList.QUIT || text == Chatr.CommandList.QUIT_S);
+            s_chatrClient?.ShutDown();
+            s_chatrClient = null;
         }
 
         /// <summary>
-        /// Display a recevied message
+        /// Start a new Chatr client using the settings from <paramref name="settingsPath"/>
         /// </summary>
-        /// <param name="message">Message to display</param>
-        private static void DisplayMessage(string message, string textColor)
+        /// <param name="settingsPath">File path to chatr settings file</param>
+        private static void StartupChatr(string settingsPath)
         {
-            ClearInputLines();
-            if (Enum.TryParse<ConsoleColor>(textColor, true, out ConsoleColor consoleColor))
+            // Create a new Chatr client
+            s_chatrClient = new Chatr.MultiChannel(settingsPath);
+
+            // Attach a message display handler
+            s_chatrClient.MessageDisplayEventHandler += (sender, m) =>
             {
-                Console.ForegroundColor = consoleColor;
-                //Console.Write($"Color is {textColor}\n>");
+                DisplayMessage(m[0], m[1]);
+            };
+        }
+
+        /// <summary>
+        /// Try to process the message as a console command
+        /// </summary>
+        /// <param name="message">Message to process</param>
+        /// <returns>true if the command was handled, otherwise false</returns>
+        private static bool TryProcessAsConsoleCommand(string message)
+        {
+            // Check for empty message
+            if (string.IsNullOrEmpty(message))
+            {
+                // Do nothing
+
+                // command handled
+                return true;
             }
-            Console.Write(message);
-            Console.ResetColor();
-            Console.Write("\n> " + s_nextInput);
+
+            // Check for the clear command
+            if (message.StartsWith("/clear", true, CultureInfo.CurrentCulture))
+            {
+                // Clear the console and display the prompt
+                Console.Clear();
+                Console.Write(Prompt);
+
+                // command handled
+                return true;
+            }
+
+            // command not handled
+            return false;
         }
 
-        /// <summary>
-        /// Display the message input from the user
-        /// </summary>
-        private static void DisplayInput()
-        {
-            // Clear the previous message input
-            ClearInputLines();
-
-            // Display the new message input
-            Console.Write("> " + s_nextInput);
-        }
-
-        /// <summary>
-        /// Clear any user message input from the display
-        /// </summary>
-        private static void ClearInputLines()
-        {
-            Console.SetCursorPosition(0, Console.CursorTop - 1 * ((s_currentInput.Length + 1) / Console.WindowWidth));
-            Console.Write(new string(' ', s_currentInput.Length + 2));
-            Console.SetCursorPosition(0, Console.CursorTop - 1 * ((s_currentInput.Length + 1) / Console.WindowWidth));
-        }
+        #endregion Private Methods
     }
 }
